161ac76bcd5356f4ef4eef336d5dc98db9c2e2e2
JaeilAn <a090066@naver.com>
2025-05-23 15:39:58 +0900

    feat: 구조화 및 창고 frame 짜기

--- src/main/java/com/team17/poc/box/repository/BoxRepository.java
+++ src/main/java/com/team17/poc/box/repository/BoxRepository.java
@@ -1,8 +1,6 @@
 package com.team17.poc.box.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BoxRepository extends JpaRepository<Box, Long> {
-    List<Box> findByMember(Member member);
+public interface BoxRepository  {
 
 }
