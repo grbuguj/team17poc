885e04bb48fbdd385d5e3de6ef35b1c9e95abd41
Jaeil An <a090066@naver.com>
2025-06-07 22:27:40 +0900

    feat: port 수정

--- src/main/java/com/team17/poc/config/SecurityConfig.java
+++ src/main/java/com/team17/poc/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
                     CorsConfiguration config = new CorsConfiguration();
                     config.setAllowedOrigins(List.of(
                             "http://localhost:3000",
-                            "http://keepbara.duckdns.org:8082"
+                            "http://keepbara.duckdns.org"
                             ));
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")); // PATCH 추가
                     config.setAllowedHeaders(List.of("*"));
