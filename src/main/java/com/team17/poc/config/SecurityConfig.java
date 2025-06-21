0131dca1eb42de40acc8eb3355a0d2b7417ac918
Jaeil An <a090066@naver.com>
2025-06-21 15:58:00 +0900

    feat: CORS 에러 수정

--- src/main/java/com/team17/poc/config/SecurityConfig.java
+++ src/main/java/com/team17/poc/config/SecurityConfig.java
@@ -31,7 +31,8 @@ public class SecurityConfig {
                             "http://localhost:5173",
                             "http://keepbara.duckdns.org",
                             "http://keepbara.duckdns.org:8082",
-                            "http://localhost:8082"
+                            "http://localhost:8082",
+                            "https://keepbara.duckdns.org"
                             ));
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")); // PATCH 추가
                     config.setAllowedHeaders(List.of("*"));
